a90812f86ad44daddaa2c5857e7dc563f3a3fc9f
Martin Hjelmare <marhje52@kth.se>
2019-02-03 03:13:03 +0100

    Change header

--- src/components/ManifestGenerator.vue
+++ src/components/ManifestGenerator.vue
@@ -3,7 +3,7 @@
     <form novalidate class="md-layout md-gutter md-alignment-center" @submit.prevent="validateUser">
       <md-card class="md-layout-item md-size-50 md-small-size-100">
         <md-card-header>
-          <div class="md-title">Users</div>
+          <div class="md-title">Model Manifest</div>
         </md-card-header>
 
         <md-card-content>
