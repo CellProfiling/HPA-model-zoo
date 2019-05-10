cd8b5066af028992a3c6ab79290b2e1b1ede83ad
Martin Hjelmare <marhje52@kth.se>
2019-05-10 22:04:39 +0200

    Fix left alignment

--- src/components/ModelAbout.vue
+++ src/components/ModelAbout.vue
@@ -38,3 +38,12 @@ export default {
   },
 };
 </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.about {
+  text-align: left;
+  padding-left: 10px;
+  padding-right: 5px;
+}
+</style>
